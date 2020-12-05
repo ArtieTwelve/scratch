6384e2861bfc37d1c5353b948e8591b031cc4d7a
Lance Smith <artieTwelve@gmail.com>
2020-12-05 12:44:52 -0500

    Next

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 
+void AddThree(int *pInt);
+
+void AddFive(bool &sucess, int &val);
+
+void AddFour(int &val) {
+  val += 4;
+}
+
 int main() {
     std::cout << "Hello, Scratch World!" << std::endl;
+    int val = 0;
+    bool sucess = false;
+    //AddThree(&val);
+    //AddFour(val);
+    AddFive(sucess, val);
+
+    std::cout << "val = " << val << std::endl;
+    std::cout << "sucess = " << sucess << std::endl;
     std::cout << "Goodbye , Scratch World!" << std::endl;
     return 0;
+}
+
+void AddFive(bool &sucess, int &val) {
+    val += 5;
+    sucess += true;
+
+}
+
+void AddThree(int *pInt) {
+    *pInt += 3;
+
 }
 
