609040678abb37103150cb5c61a4975d667d222c
Lance Smith <artieTwelve@gmail.com>
2020-12-10 13:16:08 -0500

    Cleaned out Main

---
+++ song/Song.cpp
@@ -0,0 +1,5 @@
+//
+// Created by smithla on 12/10/20.
+//
+
+#include "Song.h"
---
+++ song/Song.h
@@ -0,0 +1,14 @@
+//
+// Created by smithla on 12/10/20.
+//
+
+#ifndef SCRATCH_SONG_H
+#define SCRATCH_SONG_H
+
+
+class Song {
+
+};
+
+
+#endif //SCRATCH_SONG_H
