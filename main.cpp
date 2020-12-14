47273370da27f0e9618b79579c1cbbab858202b8
Lance Smith <artieTwelve@gmail.com>
2020-12-14 16:02:34 -0500

    Testing git

--- main.cpp
+++ main.cpp
@@ -9,26 +9,44 @@
 
 
 using namespace std;
+
+class Position {
+    int x,y;
+    std::unique_ptr<std::pair<int,int>> getPosition() {
+        return std::make_unique<std::pair<int,int>>(x,y);
+    }
+};
+
+struct BaseEntity {
+public:
+    int i;
+    std::string name;
+
+    BaseEntity(int _i, std::string _n) : i(_i), name(_n) {}
+    BaseEntity(const BaseEntity& other) {
+        i = other.i;
+        name = other.name;
+    }
+
+    void doSomething(float z, int i, int j, std::string name);
+
+    void doSomethingTwo(int i);
+
+};
+
+void BaseEntity::doSomething(float z, int i, int j, std::string name) {
+
+}
+
+
 int main() {
     std::cout << "Hello, Scratch World!" << std::endl;
-
-   struct type
-   {
-       int i;
-       type(): i(3) {}
-
-       void f(int v) const {
-          // this->i = v;
-          const_cast<type*>(this)->i = v;
-       }
-
-   };
-
 
 
 
     std::cout << "Goodbye, Scratch World!\n";
 }
+
 
 
 
